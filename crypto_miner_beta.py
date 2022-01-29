52a9a068d6c28c2eb36d2a1b3af154360f217aa1
MarkFireFox <76536605+MarkFireFox@users.noreply.github.com>
2022-01-29 18:47:11 +0000

    Add files via upload

---
+++ crypto_miner_beta.py
@@ -0,0 +1,24 @@
+from tkinter import*
+import random
+def clicked():
+    print("+0.00001 BTC")
+def click():
+    n=1000000000000000000
+    while n>0:
+        n-=1 
+        print("+0.000000001 BTC")
+    
+window=Tk()
+window.title("crypto miner")
+window.geometry('192x108')
+lbl=Label(window,text="начни майнить биткоин сейчас!",font=("Arial",10))
+lbl.grid(column=0,row=0)
+btn1=Button(window,text='автомат',command=click,fg='black',
+           bg='green',activebackground='red',
+           activeforeground='white',padx=14)
+btn1.grid(column=0,row=2)
+btn2=Button(window,text='кликер ',command=clicked,fg='black',
+           bg='green',activebackground='green',
+           activeforeground='black',padx=15)
+btn2.grid(column=0,row=1)
+window.mainloop()
